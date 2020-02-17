f7b27939bf65553f79d098577a4367d893ebdec1
Oskar Harenda <oskarharenda@gmail.com>
2020-02-17 18:25:28 +0100

    zad1

---
+++ src/oskarharenda/GenerateRandom.java
@@ -0,0 +1,31 @@
+package oskarharenda;
+
+import org.w3c.dom.ls.LSOutput;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class GenerateRandom {
+    public static void main(String[] args) {
+        int sum =0;
+        for (Map.Entry<Integer, Integer> entry : checkRand(500000,10).entrySet()) {
+            Integer key= entry.getKey();
+            Integer value = entry.getValue();
+            sum+=value;
+            System.out.println(key+" "+value);
+        }
+        System.out.println(sum);
+    }
+
+    public static Map<Integer, Integer> checkRand(int amount, int interval){
+        Map<Integer,Integer> randomNumbers = new HashMap<>();
+        Random r = new Random(interval);
+        int[] counter = new int [interval];
+        for(int i=0; i<amount; i++){
+            int rand=r.nextInt(interval);
+            randomNumbers.put(rand,counter[rand]++);
+        }
+        return  randomNumbers;
+    }
+}
---
+++ src/oskarharenda/Main.java
@@ -0,0 +1,10 @@
+package oskarharenda;
+
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
